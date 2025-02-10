d6ef28c27c6ef5cba35c4e2b2cc661a769230d9d
Gabriel Sanchez <alvarado10@gmail.com>
2025-02-10 09:14:44 -0600

    Fix font family in Safari

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,13 +4,20 @@ import type { AppProps } from 'next/app'
 // import { ToastContainer } from 'react-toastify'
 // import 'react-toastify/dist/ReactToastify.css'
 import '../styles/global.css'
+import { Baloo_2 } from 'next/font/google'
+
+const baloo = Baloo_2({
+  subsets: ['latin'],
+  weight: ['400', '700'], // Specify weights you need
+  variable: '--font-baloo' // Optional: Define a CSS variable
+})
 
 // const inter = Inter({ subsets: ['latin'] })
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <div className={baloo.className}>
       <Component {...pageProps} />
       {/* <ToastContainer theme="dark" /> */}
-    </>
+    </div>
   )
 }
