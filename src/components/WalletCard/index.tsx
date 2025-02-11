ea28c80f5be10bf08117f80cf48c0c0fc577506d
Gabriel Sanchez <alvarado10@gmail.com>
2025-02-11 15:44:47 -0600

    Round balance

--- src/components/WalletCard/index.tsx
+++ src/components/WalletCard/index.tsx
@@ -1,19 +1,19 @@
 import { CurrencyDollarSimple } from 'phosphor-react'
-import { formatPoints } from 'utils/currencyFormat'
 
 interface WalletCardProps {
   showFormatted?: boolean
   balance: number
 }
-export function WalletCard({ balance, showFormatted }: WalletCardProps) {
-  const currency = showFormatted ? formatPoints(balance) : balance
+export function WalletCard({ balance }: WalletCardProps) {
+  // const currency = showFormatted ? formatPoints(balance) : balance
+  const roundedBalance = Math.round(balance)
   return (
     <div className="flex cursor-pointer items-stretch">
       <div className="flex items-center gap-2 rounded-bl-md rounded-tl-md bg-background px-2 py-1 pr-4 font-bold uppercase text-white md:text-lg">
         <span className="rounded-full bg-purpleDark p-1">
           <CurrencyDollarSimple weight="bold" />
         </span>
-        <span title={String(balance)}>{currency}</span>
+        <span title={String(balance)}>{roundedBalance}</span>
       </div>
       <span
         title="Plinko Points"
